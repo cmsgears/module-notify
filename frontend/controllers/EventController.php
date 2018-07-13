575a51be2d1ac2ae183fe282e4fd60c85798c662
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-07-13 12:41:11 +0530

    Applied quick fixes.

---
+++ frontend/controllers/EventController.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
+namespace cmsgears\notify\frontend\controllers;
+
+// Yii Imports
+use Yii;
+use yii\filters\VerbFilter;
+
+// CMG Imports
+use cmsgears\core\frontend\controllers\base\Controller;
+
+/**
+ * EventController provides actions specific to user reminders.
+ *
+ * @since 1.0.0
+ */
+class EventController extends Controller {
+
+	// Variables ---------------------------------------------------
+
+	// Globals ----------------
+
+	// Public -----------------
+
+	// Protected --------------
+
+	// Private ----------------
+
+	// Constructor and Initialisation ------------------------------
+
+	public function init() {
+
+		parent::init();
+
+		// Services
+		$this->modelService	= Yii::$app->factory->get( 'eventService' );
+	}
+
+	// Instance methods --------------------------------------------
+
+	// Yii interfaces ------------------------
+
+	// Yii parent classes --------------------
+
+	// yii\base\Component -----
+
+	public function behaviors() {
+
+		return [
+			'rbac' => [
+				'class' => Yii::$app->core->getRbacFilterClass(),
+				'actions' => [
+					'index'	 => [ 'permission' => $this->crudPermission ],
+					'all'  => [ 'permission' => $this->crudPermission ]
+				]
+			],
+			'verbs' => [
+				'class' => VerbFilter::class,
+				'actions' => [
+					'index' => [ 'get', 'post' ],
+					'all'  => [ 'get' ]
+				]
+			]
+		];
+	}
+
+	// yii\base\Controller ----
+
+	// CMG interfaces ------------------------
+
+	// CMG parent classes --------------------
+
+	// EventController -----------------------
+
+	public function actionIndex() {
+
+		return $this->redirect( [ 'all' ] );
+	}
+
+	public function actionAll() {
+
+		$user			= Yii::$app->user->getIdentity();
+		$dataProvider	= $this->modelService->getPageByUserId( $user->id );
+
+		return $this->render( 'all', [
+			'dataProvider' => $dataProvider
+		]);
+	}
+
+}
