cbd808cd0aa1ba1d8f260a5ceb12eb0cd22cc6ea
Lokendra Chouhan <lokendra.chouhan@vulpinecode.com>
2018-10-02 10:59:35 +0530

    unit test setup

---
+++ frontend/tests/unit/services/resources/HelloTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace cmsgears\notify\frontend\tests\unit\services\resources;
+
+//use common\fixtures\UserFixture;
+//use common\models\entities\CashbackActivity;
+/**
+ * Login form test
+ */
+class helloTest extends \Codeception\Test\Unit
+{
+	public function testHello(){
+		
+		expect( "hello", true )->true();
+	}
+}
